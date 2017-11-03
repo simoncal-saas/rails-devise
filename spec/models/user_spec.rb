c220afb066bfa8da76ca3d4b532ad3fc2988defa
simoncal-saas <simoncal@yahoo.com>
2017-11-02 23:34:31 -0700

    add name, first & last name to unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,17 @@
 describe User do
 
-  before(:each) { @user = User.new(email: 'user@example.com') }
+  before(:each) { @user = User.new(email: 'user@example.com', first_name: 'Jack', last_name: 'Hardy') }
 
   subject { @user }
 
-  it { should respond_to(:email) }
+  it { should respond_to(:email, :first_name, :last_name) }
 
   it "#email returns a string" do
     expect(@user.email).to match 'user@example.com'
+  end
+
+  it "returns a full name" do
+    expect(@user.name).to match 'Jack Hardy'
   end
 
 end
